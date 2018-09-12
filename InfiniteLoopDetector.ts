d98c38f42b2748b0eef5cd3af09548e42667fab6
lamyoung <273404202@qq.com>
2018-09-12 11:22:15 +0800

    Update InfiniteLoopDetector.ts

--- InfiniteLoopDetector.ts
+++ InfiniteLoopDetector.ts
@@ -3,24 +3,28 @@ module DebugUtil {
 		private constructor() {
 
 		}
-		private static map = {};
+		private static map: { [index: number]: { count: number, time: number } } = {};
 		private static _id = 1;
+		public static get id() {
+			return this._id++ % Number.MAX_VALUE;
+		}
 
-		private static infiniteLoopDetector(id: number) {
+		public static infiniteLoopDetector(id: number) {
 			if (id in this.map) { // 非首次执行，此处可以优化，性能太低
-				if (Date.now() - this.map[id] > 3000) {
+				if ((Date.now() - this.map[id].time > 3000) && this.map[id].count > 10000) {
 					delete this.map[id]
 					throw new Error('Loop runing too long!')
 				}
+				this.map[id].count++;
 			} else { // 首次运行，记录循环开始的时间。之所有把非首次运行的判断写在前面的if里是因为上面会执行更多次
-				this.map[id] = Date.now()
+				this.map[id] = { count: 0, time: Date.now() }
 			}
 		}
 
 		private static wrap = function (codeStr: string) {
 			// this is not a strong regex, but enough to use at the time
 			return codeStr.replace(/for *\(.*\{|while *\(.*\{|do *\{/g, (loopHead) => {
-				const id = this._id++ % Number.MAX_VALUE;
+				const id = DebugUtil.InfiniteLoopDetector.id;
 				return `DebugUtil.InfiniteLoopDetector.infiniteLoopDetector(${id});${loopHead}DebugUtil.InfiniteLoopDetector.infiniteLoopDetector(${id});`
 			})
 		}
